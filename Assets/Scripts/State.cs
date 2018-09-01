96692655999472dd34dfcee835991abef48296e7
Ghislain Rodrigues <git@ghislain-rodrigues.fr>
2018-09-01 23:42:23 +0100

    Assets reorganised and missing states added

--- Assets/Scripts/State.cs
+++ Assets/Scripts/State.cs
@@ -5,6 +5,13 @@ using UnityEngine;
 // Makes this object creatable in Unity with right click -> create
 [CreateAssetMenu(menuName = "State")]
 public class State : ScriptableObject {
+    /*
+     * create SceneState and SceneStateCondition classes (a scene state has a condition)
+     * Rename into StoryScene (A scene has multiple states and a single active)
+     * When initialising scene, go through its states and find the first for which the condition is valid and set it as active
+     * 
+     * 
+     */
     [SerializeField] string storyTitle;
     [TextArea(10, 14)] [SerializeField] string storyText;
     [SerializeField] State[] linkedStates;
