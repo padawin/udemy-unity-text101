a91da473f0d4bbd1f007f5c8374093ffea27cec9
Ghislain Rodrigues <git@ghislain-rodrigues.fr>
2018-09-02 23:06:50 +0100

    Action to change scene added

---
+++ Assets/Scripts/Actions/ChangeSceneAction.cs
@@ -0,0 +1,11 @@
+﻿using UnityEngine;
+
+[CreateAssetMenu(menuName = "ChangeSceneAction")]
+public class ChangeSceneAction : SceneAction {
+	[SerializeField] SceneManager sceneManager;
+    [SerializeField] StoryScene newScene;
+
+    override public void execute() {
+        sceneManager.setScene(newScene);
+    }
+}
